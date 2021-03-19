85f7e6d9d5c3e2d643dad12dc34e1018d1969aa0
Peter Kahle <petkahl@microsoft.com>
2021-03-19 10:31:35 -0400

    Adding suppressions to make it compile.

--- tests/Microsoft.AzureIntegrationMigration.Runner.Tests/GlobalSuppressions.cs
+++ tests/Microsoft.AzureIntegrationMigration.Runner.Tests/GlobalSuppressions.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation.
+﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 // This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
@@ -10,3 +10,4 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "This is test project.", Scope = "namespaceanddescendants", Target = "Microsoft.AzureIntegrationMigration.Runner.Tests")]
 [assembly: SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task", Justification = "This is test project.", Scope = "namespaceanddescendants", Target = "Microsoft.AzureIntegrationMigration.Runner.Tests")]
 [assembly: SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "This is test project.", Scope = "namespaceanddescendants", Target = "Microsoft.AzureIntegrationMigration.Runner.Tests")]
+[assembly: SuppressMessage("Style", "IDE0073:The file header does not match the required text", Justification = "<Pending>")]
